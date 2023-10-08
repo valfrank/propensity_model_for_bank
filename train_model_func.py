10ccf38a9d82e2c881338c5e7578aaa5b2402612
Valeriia Rostovtseva <94761081+valfrank@users.noreply.github.com>
2023-10-08 14:31:52 +0400

    Update train_model_func.py

--- train_model_func.py
+++ train_model_func.py
@@ -1,57 +1,70 @@
 import pickle
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import catboost
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 my_colors = ['#2350D9', '#417CF2', '#25D997', '#96D9B3', '#F2C84B']
 plt.style.use('dark_background')
 
+HERE = ''
 
 def load_from_pkl(path='data', file_name='model'):
-    """ Load saved model as pickle file"""
-    with open(f'{path}/{file_name}.pickle', 'rb') as f:
+    """
+    Load saved model as pickle file
+    """
+    with open(f'{HERE}/{path}/{file_name}.pickle', 'rb') as f:
         loading = pickle.load(f)
     return loading
 
 
+def save_to_pkl(data, path='data', file_name='model'):
+    """
+    Save oblects as pickle files
+    """
+    with open(f'{HERE}/{path}/{file_name}.pickle', 'wb') as f:
+        pickle.dump(data, f)
+
+
 def predict_on_input(df: pd.DataFrame):
-    """ Load model and returns probability"""
+    """
+    Load model and returns probability
+    """
     model = load_from_pkl()
     pred = model.predict_proba(df)
     return pred
 
 
 def preprocess_data(data):
-    """ Function for scaling and encoding data"""
+    """
+    Function for scaling and encoding data
+    """
     col_transformer = load_from_pkl(path='data', file_name='col_transformer')
     X = col_transformer.transform(data)
     return X
 
 
-def visualisation_metrics(X, y, i):
+def train_model(data: pd.DataFrame, classifier: str):
     """
-    Create table with metrics anв confusion matrix plot depending on threshold
+    Function that take model and parameters from models dictionary and train it on data
+    :param data: pd.DataFrame
+    :param classifier: name of model
     """
-    model = load_from_pkl()
-    test_pred = model.predict_proba(X)
-    classes = test_pred[:, 1] > i
-    accuracy = accuracy_score(y, classes)
-    recall = recall_score(y, classes)
-    presicion = precision_score(y, classes)
-    f1 = f1_score(y, classes)
-    metrics_table = pd.DataFrame({f'Treshold = {i}': [accuracy, recall, presicion, f1]},
-                                 index=['Accuracy', 'Recall', 'Precision', 'F1-score'])
-    matrix = confusion_matrix(y, classes)
-    group_names = ['True Neg', 'False Pos', 'False Neg', 'True Pos']
-    group_counts = ['{0:0.0f}'.format(value) for value in matrix.flatten()]
-    group_percentages = ['{0:.2%}'.format(value) for value in matrix.flatten() / np.sum(matrix)]
-    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
-    labels = np.asarray(labels).reshape(2, 2)
-    fig = plt.figure(figsize=(4, 4))
-    sns.heatmap(matrix, annot=labels, fmt='', cmap=my_colors, annot_kws={"size": 12}, square=True)
-    plt.title('Confusion matrix', size=16, weight="bold")
-    plt.show()
-    return metrics_table, fig
+    models = {
+        'LogisticRegression': LogisticRegression(C=0.001, penalty=None, solver='lbfgs'),
+        'CatBoost': CatBoostClassifier(learning_rate=0.1, loss_function='Logloss', eval_metric='Accuracy',
+                                       iterations=500,
+                                       max_depth=4, l2_leaf_reg=3, ),
+        'SVM': SVC(probability=True, C=10, kernel='sigmoid')
+    }
+
+    X = data.drop(['id', 'target'], axis=1)
+    X = preprocess_data(X)
+    y = data['target']
+    model = models[classifier]
+    model.fit(X, y)
+
+    save_to_pkl(model, file_name=f"{classifier}-model")
+
+
